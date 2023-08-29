1e59f8706a1d7286580d939320fb2ac53f3f00d8
Josh Madison <jmad99@sbcglobal.net>
2023-08-29 10:03:08 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Josh Madison on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
