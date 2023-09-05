d18299dcf7b9d9258774ce42d3a60b21cccb9bdd
Josh Madison <jmad99@sbcglobal.net>
2023-09-05 09:23:15 -0500

    Added images and prices for items on the menu

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,27 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Josh Madison on 9/5/23.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    let item: MenuItem
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
