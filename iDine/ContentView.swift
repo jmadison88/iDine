243261d72e3bb1b56a33ffd04d365f4664711fc8
Josh Madison <jmad99@sbcglobal.net>
2023-09-10 18:57:26 -0500

    Created a view for individual menu items

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -10,15 +10,20 @@ import SwiftUI
 struct ContentView: View {
     let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
     var body: some View {
-        NavigationView {
+        NavigationStack {
             List {
                 ForEach(menu) { section in
                     Section(section.name) {
                         ForEach(section.items) { item in
-                            ItemRow(item: item)
+                            NavigationLink(value: item) {
+                                ItemRow(item: item)
+                            }
                         }
                     }
                 }
+            }
+            .navigationDestination(for: MenuItem.self) { item in
+                ItemDetail(item: item)
             }
             .navigationTitle("Menu")
             .listStyle(GroupedListStyle())
