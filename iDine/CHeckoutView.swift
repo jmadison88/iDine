9ae723f8388c2eb6157d07745b6fa108207f51c8
Josh Madison <jmad99@sbcglobal.net>
2023-09-10 19:39:13 -0500

    Added a total price when checking out

--- iDine/CHeckoutView.swift
+++ iDine/CHeckoutView.swift
@@ -15,6 +15,11 @@ struct CheckoutView: View {
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 15
     let tipAmounts = [10,15,20,25,0]
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
     var body: some View {
         VStack {
             Section {
@@ -36,7 +41,7 @@ struct CheckoutView: View {
                 }
                 .pickerStyle(.segmented)
             }
-            Section("Total: $100") {
+            Section("Total: \(totalPrice)") {
                 Button("Confirm order") {
                     
                 }
