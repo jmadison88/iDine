1fddc1d49c70867ae3fbe3f5ef8c9898d3840ebf
Josh Madison <jmad99@sbcglobal.net>
2023-09-10 19:28:48 -0500

    Added options for OrderView

--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct OrderView: View {
     @EnvironmentObject var order: Order
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
+    let tipAmounts = [10,15,20,25,0]
     var body: some View {
         NavigationStack {
             List {
@@ -24,7 +28,24 @@ struct OrderView: View {
                 
                 Section {
                     NavigationLink("Place Order") {
-                        Text("Check out")
+                        CheckoutView()
+                    }
+                    Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                    if addLoyaltyDetails {
+                        TextField("Enter your iDine ID", text: $loyaltyNumber)
+                    }
+                }
+                Section("Add a tip?") {
+                    Picker("Percentage:", selection: $tipAmount) {
+                        ForEach(tipAmounts, id:\.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Total: $100") {
+                    Button("Confirm order") {
+                        
                     }
                 }
             }
